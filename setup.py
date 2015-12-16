fc08b1d5efe93626943d19eb1994abe3934529f9
tek <torstenschmits@gmail.com>
2015-12-17 00:32:30 +0100

    2.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages  # type: ignore
 setup(
     name='proteome',
     description='project management for neovim',
-    version='0.2.3',
+    version='2.0.0',
     author='Torsten Schmits',
     author_email='torstenschmits@gmail.com',
     license='MIT',
