a3fcb6c37b53ed9e096ffed28e1f5f13adcc707f
tek <torstenschmits@gmail.com>
2015-12-20 02:02:23 +0100

    refactor vim.chdir into NvimFacade.cd

--- proteome/nvim.py
+++ proteome/nvim.py
@@ -12,9 +12,12 @@ class NvimFacade(Logging, trypnv.nvim.NvimFacade):
     def __init__(self, nvim: neovim.Nvim) -> None:
         trypnv.nvim.NvimFacade.__init__(self, nvim, 'proteome')
 
+    def cd(self, d):
+        self.vim.chdir(str(d))
+
     def switch_root(self, path):
         p = str(path)
-        self.vim.chdir(p)
+        self.cd(p)
         self.pautocmd('SwitchedRoot')
         self.set_pvar('root', p)
         self.set_pvar('root_name', str(path.name))
