bafe0ae07bc69af45b24fd669cffe70f58ae13e7
tek <torstenschmits@gmail.com>
2016-02-24 00:53:05 +0100

    bump trypnv dep

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     packages=find_packages(
         exclude=['unit', 'unit.*', 'integration', 'integration.*']),
     install_requires=[
-        'tryp-nvim>=5.1.0',
+        'tryp-nvim>=5.1.1',
         'pygit2',
     ]
 )
