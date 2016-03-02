12d841cecaf11dfaf97e642f51ffa9264065b213
tek <torstenschmits@gmail.com>
2016-03-02 17:35:01 +0100

    5.4.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages  # type: ignore
 setup(
     name='proteome',
     description='project management for neovim',
-    version='5.4.0',
+    version='5.4.1',
     author='Torsten Schmits',
     author_email='torstenschmits@gmail.com',
     license='MIT',
@@ -11,7 +11,7 @@ setup(
     packages=find_packages(
         exclude=['unit', 'unit.*', 'integration', 'integration.*']),
     install_requires=[
-        'tryp-nvim>=5.3.0',
+        'tryp-nvim>=5.4.0',
         'pygit2',
     ]
 )
