63b2722b9081910b5ce92f233f6d92fc84bbfbbf
tek <torstenschmits@gmail.com>
2016-03-18 00:26:58 +0100

    refactor ctags plugin

--- proteome/plugins/ctags.py
+++ proteome/plugins/ctags.py
@@ -1,13 +1,12 @@
 from fn import _
 
-from trypnv.machine import may_handle, message, handle
+from trypnv.machine import may_handle, message
 from trypnv.nvim import Buffer
 
 from tryp import List, Empty
 from tryp.lazy import lazy
 
-from proteome.state import ProteomeComponent
-from proteome.env import Env
+from proteome.state import ProteomeComponent, ProteomeTransitions
 from proteome.ctags import Ctags
 from proteome.plugins.core import Save, Added, BufEnter, StageIV
 
@@ -19,57 +18,59 @@ CurrentBuffer = message('CurrentBuffer')
 
 class Plugin(ProteomeComponent):
 
-    @property
-    def ctags(self) -> Ctags:
-        return self._ctags  # type: ignore
-
     @lazy
-    def _ctags(self):
+    def ctags(self):
         return Ctags(self.vim)
 
-    @property
-    def _tags_file_name(self):
-        return '.tags'
+    class Transitions(ProteomeTransitions):
 
-    @handle(Gen)
-    async def gen(self, env, msg):
-        await self.ctags.gen(msg.project)
-        return Empty()
+        @property
+        def ctags(self) -> Ctags:
+            return self.machine.ctags  # type: ignore
 
-    @may_handle(StageIV)
-    def stage_4(self, env: Env, msg):
-        return GenAll(), CurrentBuffer()
+        @property
+        def _tags_file_name(self):
+            return '.tags'
 
-    @may_handle(Save)
-    def save(self, env: Env, msg):
-        return GenAll()
+        @may_handle(StageIV)
+        def stage_4(self):
+            return GenAll(), CurrentBuffer()
 
-    @may_handle(GenAll)
-    def gen_all(self, env: Env, msg):
-        if self.ctags.ready:
-            return env.projects.projects\
-                .filter(_.want_ctags)\
-                .map(Gen)
+        @may_handle(Save)
+        def save(self):
+            return GenAll()
 
-    # TODO kill dangling procs
-    @may_handle(Kill)
-    def kill(self, env: Env, msg):
-        pass
+        @may_handle(GenAll)
+        def gen_all(self):
+            if self.ctags.ready:
+                return self.data.projects.projects\
+                    .filter(_.want_ctags)\
+                    .map(Gen)
 
-    @may_handle(BufEnter)
-    def buf_enter(self, env, msg):
-        self.set_buffer_tags(env, List(msg.buffer))
+        # TODO kill dangling procs
+        @may_handle(Kill)
+        def kill(self):
+            pass
 
-    @may_handle(Added)
-    def added(self, env: Env, msg: Added):
-        return CurrentBuffer()
+        @may_handle(BufEnter)
+        def buf_enter(self):
+            self.set_buffer_tags(List(self.msg.buffer))
 
-    @may_handle(CurrentBuffer)
-    def buffer(self, env: Env, msg):
-        self.set_buffer_tags(env, List(self.vim.buffer))
+        @may_handle(Added)
+        def added(self: Added):
+            return CurrentBuffer()
 
-    def set_buffer_tags(self, env: Env, bufs: List[Buffer]):
-        files = env.all_projects.map(_.root / self._tags_file_name)
-        bufs.foreach(lambda a: a.amend_optionl('tags', files))
+        @may_handle(CurrentBuffer)
+        def buffer(self):
+            self.set_buffer_tags(List(self.vim.buffer))
+
+        def set_buffer_tags(self, bufs: List[Buffer]):
+            files = self.data.all_projects.map(_.root / self._tags_file_name)
+            bufs.foreach(lambda a: a.amend_optionl('tags', files))
+
+        @may_handle(Gen)
+        async def gen(self):
+            await self.ctags.gen(self.msg.project)
+            return Empty()
 
 __all__ = ('GenAll', 'Plugin')
