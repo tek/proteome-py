275b2616c029bad1f908f20d99d3fd5ed158321e
tek <torstenschmits@gmail.com>
2016-08-19 18:49:58 +0200

    fix machine constructor args

--- proteome/state.py
+++ proteome/state.py
@@ -1,25 +1,23 @@
-from ribosome import Machine, PluginStateMachine
+from ribosome import Machine, RootMachine
 from ribosome.nvim import HasNvim
 from ribosome.machine import ModularMachine, Transitions
 
 from proteome.nvim import NvimFacade
 from proteome.logging import Logging
 
-from amino import List
-
 
 class ProteomeComponent(ModularMachine, HasNvim, Logging):
 
     def __init__(self, name: str, vim: NvimFacade, parent=None) -> None:
-        Machine.__init__(self, name, parent)
+        Machine.__init__(self, parent)
         HasNvim.__init__(self, vim)
 
 
-class ProteomeState(PluginStateMachine, HasNvim, Logging):
+class ProteomeState(RootMachine, Logging):
 
-    def __init__(self, vim: NvimFacade, plugins: List[str]) -> None:
-        HasNvim.__init__(self, vim)
-        PluginStateMachine.__init__(self, 'proteome', plugins)
+    @property
+    def name(self):
+        return 'proteome'
 
 
 class ProteomeTransitions(Transitions, HasNvim):
