05a4b5a1a1ba6abec3c96d94e35e2bdc35a9e6ca
tek <torstenschmits@gmail.com>
2016-08-22 13:18:10 +0200

    make `ProteomeNvimPlugin.state` a property

--- proteome/nvim_plugin.py
+++ proteome/nvim_plugin.py
@@ -62,6 +62,7 @@ class ProteomeNvimPlugin(NvimStatePlugin, Logging):
         self.pro = None
         self._post_initialized = False
 
+    @property
     def state(self):
         return self.pro
 
