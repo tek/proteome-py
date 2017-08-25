078f669fa75189e1963ba7f2f048c005ce4c0527
Torsten Schmits <torstenschmits@gmail.com>
2017-08-26 01:18:03 +0200

    10.0.7

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version_parts = (10, 0, 6)
+version_parts = (10, 0, 7)
 version = '.'.join(map(str, version_parts))
 
 setup(
@@ -14,7 +14,7 @@ setup(
     packages=find_packages(
         exclude=['unit', 'unit.*', 'integration', 'integration.*']),
     install_requires=[
-        'ribosome==10.10.3',
+        'ribosome==10.11.1',
         'dulwich',
     ]
 )
